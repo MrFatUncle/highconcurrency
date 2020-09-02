c986e7a96240785e7acbf4f5dc92551ac3d56107
liaocm <122789454@qq.com>
2020-09-02 23:02:24 +0800

    init

---
+++ src/main/java/cn/lcm/highconcurrency/gift/controller/GiftController.java
@@ -0,0 +1,11 @@
+/**
+ * FileName: GiftController
+ * Author:   liaocm
+ * Date:     2020/9/2 23:02
+ * Description:
+ * History:
+ */
+package cn.lcm.highconcurrency.gift.controller;
+
+public class GiftController {
+}
