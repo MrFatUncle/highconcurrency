3fd2b228699fe9f667625542af8795a5e21f3870
liaocm <122789454@qq.com>
2020-09-02 23:00:39 +0800

    init

---
+++ src/main/java/cn/lcm/highconcurrency/HighconcurrencyApplication.java
@@ -0,0 +1,13 @@
+package cn.lcm.highconcurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighconcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighconcurrencyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/lcm/highconcurrency/high/controller/HighController.java
@@ -0,0 +1,17 @@
+/**
+ * FileName: HighController
+ * Author:   liaocm
+ * Date:     2020/9/2 22:59
+ * Description:
+ * History:
+ */
+package cn.lcm.highconcurrency.high.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/highCon/high")
+public class HighController {
+
+}
